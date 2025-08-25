8c6633ac5351f10f0c3d9dfc2954bc98f66fc2b4
bhavishya7 <71849128+bhavishya7@users.noreply.github.com>
2025-08-24 21:12:00 -0500

    initial setup complete

---
+++ src/Popup.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Popup: React.FC = () => {
+  return (
+    <div className="p-4 m-4">
+      <h1 className="text-lg font-bold">HELLO</h1>
+    </div>
+  );
+};
+
+export default Popup;
